73993e879c0c06632e49b9c7592f0bf74e68bb55
Ritik Raj <iamtheritikraj@gmail.com>
2024-11-09 20:27:01 +0530

    updated

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'http://localhost:8000',
-  withCredentials: true, // Ensure credentials (like cookies) are sent with requests
+  withCredentials: true,
 });
 
 export default instance;
